2b9fe899cd067574730f0c9a6ddc517a0f2eb590
tk0358 <tk0358a@yahoo.co.jp>
2019-01-23 13:29:00 +0900

    finish 5-8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'cabybara/rspec'
+require 'capybara/rspec'
 
 
 RSpec.configure do |config|
---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+# require 'selenium-webdriver'
+# require 'nokogiri'
+# #
+# options = Selenium::WebDriver::Chrome::Options.new
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')
+#
+# driver = Selenium::WebDriver.for :chrome, options: options
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    end
+
+    context 'ユーザーAがログインしているとき' do
+      before do
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+  end
+end
