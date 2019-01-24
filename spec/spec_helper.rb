6ee8fdfd1bc0cd2dd65e22929aab837817cb27c7
tk0358 <tk0358a@yahoo.co.jp>
2019-01-24 13:31:55 +0900

    環境構築試行錯誤による修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@
 require 'capybara/rspec'
 
 
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium_chrome_headless
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 # require 'selenium-webdriver'
 # require 'nokogiri'
 # #
