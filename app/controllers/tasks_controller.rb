5c7602455ee0794aff4ad14f5a6feec1cd6e3f0e
tk0358 <tk0358a@yahoo.co.jp>
2019-01-27 21:54:27 +0900

    Add retrieval in index page (finish 7-2)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = current_user.tasks.order(created_at: :desc)
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true).recent
   end
 
   def show
@@ -23,7 +24,7 @@ class TasksController < ApplicationController
       render :new
       return
     end
-    
+
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
