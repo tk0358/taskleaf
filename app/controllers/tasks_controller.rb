6ac0d02b375229c240aff55e524b5d660057d6f1
tk0358 <tk0358a@yahoo.co.jp>
2019-01-27 18:30:46 +0900

    add confirm_page (finish 7-1)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,9 +11,19 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
 
+    if params[:back].present?
+      render :new
+      return
+    end
+    
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
